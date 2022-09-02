436440a78d359bc76d67d1b37ff9e78b91d00342
ListowelAdolwin <listoweladolwin@gmail.com>
2022-09-02 23:02:00 +0000

    Console. Added do_updated and updated do_all

--- console.py
+++ console.py
@@ -7,6 +7,12 @@ import cmd
 from models.base_model import BaseModel
 from models import storage
 import json
+import shlex
+from models.user import User
+#from models.city import City
+#from models.state import State
+#from models.place import Place
+#from models.revier import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -14,7 +20,7 @@ class HBNBCommand(cmd.Cmd):
     Class to implement the basic commands of
     the interpreter
     """
-    classes_list = ['BaseModel']
+    classes_list = ['BaseModel', 'User']
 
     prompt = '(hbnd) '
 
@@ -31,20 +37,20 @@ class HBNBCommand(cmd.Cmd):
         print("quit command to exit the interpreter\n")
 
     def emptyline(self):
-        pass
+        return False
     
     def do_create(self, line):
         if not line:
             print("** class name missing **")
-            return
+            return False
 
         args = line.split()
 
         if args[0] not in HBNBCommand.classes_list:
             print("** class doesn't exist **")
-            return
+            return False
 
-        if len(args) == 1:
+        if len(args) >= 1:
             obj = eval(args[0])()
             obj.save()
             print(obj.id)
@@ -55,28 +61,29 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, line):
         if not line:
             print("** class name missing **")
-            return
+            return False
 
-        args = line.split()
+        args = shlex.split(line)
         
         if args[0] not in HBNBCommand.classes_list:
             print("** class doesn't exist **")
-            return
+            return False
 
         if len(args) < 2:
             print("** instance id missing **")
-            return
+            return False
         
-        reloaded_json = storage.reload()
-        print(reloaded_json)
-        
+        storage.reload()
+        reloaded_json = storage.all()
+
         key = ".".join(args)
         if key not in reloaded_json.keys():
             print("** instance not found **")
-            return
+            return False
+
         else:
             print(reloaded_json[key])
-            return
+            return False
     
     def help_show(self):
         print("Use show with a class name and instance id to display the instance\n")
@@ -84,23 +91,25 @@ class HBNBCommand(cmd.Cmd):
     def do_destroy(self, line):
         if not line:
             print("** class name missing **")
-            return
+            return False
 
         args = line.split()
 
         if args[0] not in HBNBCommand.classes_list:
             print("*** class doesn't exist ***")
-            return
+            return False
 
         if len(args) < 2:
             print("*** instance id missing ***")
-            return
+            return False
 
-        reloaded_json = storage.reload()
+        storage.reload()
+        reloaded_json = storage.all()
+        
         key = ".".join(args)
         if key not in reloaded_json:
             print("*** instance not found ***")
-            return
+            return False
 
         else:
             del reloaded_json[key]
@@ -110,21 +119,69 @@ class HBNBCommand(cmd.Cmd):
         print("Use destroy with a class name and instance id to delete the instance\n")
 
     def do_all(self, line):
-        reloaded_json = storage.reload()
+        
+        new_list = []
+        storage.reload()
+        reloaded_json = storage.all()
 
         if not line:
             for key in reloaded_json.keys():
-                print(reloaded_json[key])
-            return
-        args = line.split()
+                print(str(reloaded_json[key]))
+            return False
+
+        args = shlex.split(line)
         if args[0] not in HBNBCommand.classes_list:
             print("** class doesn't exist **")
+
         else:
-            for key in reloaded_json.keys():
-                print(reloaded_json[key])
-                
+            for key, value in reloaded_json.items():
+                if type(value) is eval(args[0]):
+                    print(str(reloaded_json[key]))
+                else:
+                    return False
+                    
     def help_all(self):
-        print(" Use help with or without arguments to print all attributes")
+        print(" Use all with or without arguments to print all attributes")
+
+    def do_update(self, line):
+
+        storage.reload()
+        relooaded_json = storage.all()
+        args = shlex.split(line)
+        if len(args) < 1:
+            print("** class name missing **")
+
+        elif len(args) < 2:
+            print("** instance id is missing **")
+
+        elif len(args) < 3:
+            print("** attribute name missing **")
+
+        elif len(args) < 4:
+            print("** value missing **")
+        
+        elif args[0] not in HBNBCommand.classes_list:
+            print("** class doesn't exist **")
+        
+        elif (args[0]+"."+args[1]) not in relooaded_json.keys():
+            print("** instance doesn't exist")
+
+        elif (args[2]) in ["created_at", "id", "updated_at"]:
+            print(f"** can't update {args[2]}")
+            return False
+            
+        else:
+            key = ".".join(args[2:4])
+            key = args[0] + "." + args[1]
+            setattr((relooaded_json[key]), args[2], args[3])
+            storage.all()[key].save()
+            return False
+
+    def help_update(self):
+        print("Use update to update attributes")
+
+
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
