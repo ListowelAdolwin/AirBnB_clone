25ade8436daa27b40a39a35a80b328c22c5603c3
ListowelAdolwin <listoweladolwin@gmail.com>
2022-09-03 21:47:59 +0000

    Added the amenity class

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Handles all amenities
+"""
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """
+    Class to deal with all amenities instances
+    """
+    name = ""
