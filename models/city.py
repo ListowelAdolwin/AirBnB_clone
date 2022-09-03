5f6592a225a15327dc7bda8f2fa3dd7896b1c4dc
ListowelAdolwin <listoweladolwin@gmail.com>
2022-09-03 21:41:55 +0000

    City class

---
+++ models/city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Handles everything about City
+"""
+
+from models.base_model import BaseModel
+from models.state import State
+
+
+class City(BaseModel):
+    """
+    Class to deal with all City attributes
+    """
+    state_id = State.id
+    name = ""
